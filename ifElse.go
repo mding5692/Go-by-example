b8edacb4ec7fee3bac533b60500e27c9ba6bcf99
Michael <m.ding5692@gmail.com>
2015-06-30 14:21:55 -0400

    Create ifElse.go

---
+++ ifElse.go
@@ -0,0 +1,21 @@
+package main 
+
+import "fmt"
+
+// just testing out a basic if-else statement, nothing fancy
+
+func main() {
+
+var i = true
+
+// no brackets again
+if i == true {
+  fmt.Println(i)
+  } else {
+  fmt.Println(!i)
+  }
+  
+  // didn't use an else if here, didn't feel it was necessary
+
+
+}

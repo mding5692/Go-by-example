8bfa305aae1a02a0a7d8079c4797dfd41753937e
Michael <m.ding5692@gmail.com>
2015-06-30 14:16:24 -0400

    Update and rename mathAndConst.go to const.go

---
+++ const.go
@@ -0,0 +1,19 @@
+
+package main
+
+import "fmt" 
+import "math"
+
+const constant int = 1
+
+func main() {
+  
+  fmt.Println(constant)
+  
+  // numeric constants expect a cast such as int64 or float64 
+  const num = 10000
+  
+  fmt.Println(int64(num))
+  
+  
+}

38c8d7a2c8a712b40a796ee1575c974d4397ac18
Michael <m.ding5692@gmail.com>
2015-06-30 12:02:33 -0400

    Update and rename secondProg.go to valuesAndVariables.go

---
+++ valuesAndVariables.go
@@ -0,0 +1,22 @@
+
+package main
+
+// don't forget the input/output library
+import "fmt"
+
+// nor your main function
+func main() {
+  
+  fmt.Println("this is a number:", 2+2) 
+  // would print out "this is a number: 4"
+  
+  fmt.Println("Strings" + "can" + "be" + "concatenated" + "together")
+  
+  // booleans still the same
+  fmt.Println(true && false)
+  fmt.Println(true || false)
+  fmt.Println(!true
+  
+  
+  
+}

64c3523238c7bd232034ad2013f39bc312be8128
Michael <m.ding5692@gmail.com>
2015-06-30 14:19:25 -0400

    Update loops.go

--- loops.go
+++ loops.go
@@ -3,8 +3,12 @@ package main
 import "fmt"
 
 func main {
-// creating a for loop, as you can see, no brackets needed
-
+// creating a for loop
+  // as you can see, no brackets needed
+  // but one still needs semi-colons to separate it up
+  for i := 0; i <= 5; i++ {
+    fmt.Println(i)
+  }
 
 
 }

9a61a31ac67363253bb84d9fd70668ef4c5769c0
Michael <m.ding5692@gmail.com>
2015-06-30 13:03:25 -0400

    Update valuesAndVariables.go

--- valuesAndVariables.go
+++ valuesAndVariables.go
@@ -17,7 +17,26 @@ func main() {
   fmt.Println(true || false)
   fmt.Println(!true)
   
+  // reminds me of Javascript variables
+  var str string = "This is a string variable and this is the number "
   
+  // and yep, those are data types behind the variable name
+  var num int = 1
+  
+  // boolean seems the same
+  var truth = true
+  
+  // printing it out now
+  fmt.Println(str + num)
+  
+  // without any value binded to a int variable, it will be 0
+  var initNum int
+  if (initNum == 0 ) { fmt.Println(truth) }
+  // ^ did a decision up there
+  
+  shortHand := "short way of declaring variable"
+  // equivalent to saying:
+  // var shortHand string = "short way ..."
   
 }
 

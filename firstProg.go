0ac1a27fcd942005b5aa472f145dd865dda2e93b
Michael <m.ding5692@gmail.com>
2015-06-30 11:54:56 -0400

    Update and rename firstTry.go to firstProg.go

---
+++ firstProg.go
@@ -0,0 +1,27 @@
+
+  
+  package main 
+  
+  // imports the fmt library which is go's standard library for input and output
+  import "fmt" 
+  
+  // main function declaration
+  func main() {
+    // no semi-colons
+    // this prints the words "hello world"
+    fmt.Println("hello world")
+    // impressive, no, has support for asian letters 
+    fmt.Println("Hello, 世界")
+    
+  }
+  
+  
+  /* Steps for compiling the above program 
+  
+  go run hello-world.go
+  go build hello-world.go
+  ./hello-world
+  
+  */
+  
+  // Also comments are the same as most programming languages, uses // and /* ... */
